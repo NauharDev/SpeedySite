064c9d6267f6866e94a6d89b25107fd0590d2e51
NauharDev <nauhar.kapur@mail.utoronto.ca>
2025-01-05 14:25:30 -0500

    window not defined bug fix
    wrapped references to window variable in useEffect hook to ensure the code is executed after the component mounts in the browser.

--- speedysite/src/app/home/page.tsx
+++ speedysite/src/app/home/page.tsx
@@ -1,31 +1,38 @@
 "use client"
 
+import { useEffect } from "react";
+
 export default function Home() {
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const authorizationCode = urlParams.get("code");
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const authorizationCode = urlParams.get("code");
+        if (typeof window !== "undefined") {
+            if (authorizationCode) {
+                console.log("Authorization Code:", authorizationCode);
+                fetch(process.env.NEXT_PUBLIC_TOKEN_EXCHANGE_URL as string, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ code: authorizationCode }),
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log("Token exchange successful:", data);
+                        // Handle tokens (e.g., save access token in local storage or cookies)
+                    })
+                    .catch((error) => {
+                        console.error("Token exchange failed:", error);
+                    });
+    
+            } else {
+                console.error("Authorization code not found in URL.");
+            }
+        }
+        
+    }, [])
 
-    if (authorizationCode) {
-        console.log("Authorization Code:", authorizationCode);
-        fetch(process.env.NEXT_PUBLIC_TOKEN_EXCHANGE_URL as string, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ code: authorizationCode }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("Token exchange successful:", data);
-                // Handle tokens (e.g., save access token in local storage or cookies)
-            })
-            .catch((error) => {
-                console.error("Token exchange failed:", error);
-            });
-
-    } else {
-        console.error("Authorization code not found in URL.");
-    }
 
     return (
         <div>Logged in!</div>
